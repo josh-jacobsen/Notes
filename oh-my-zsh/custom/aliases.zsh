d2d3091fcd1961728b8d02863c27daa10ada91a8
Josh Jacobsen <josh.dylan.jacobsen@gmail.com>
2022-06-26 13:59:55 +1200

    Create aliases.zsh

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,7 @@
+# Terminal
+alias cdk='cd ~/vital/katoa'
+alias cdd='cd ~/dev'
+alias listAlias='cat ~/.oh-my-zsh/custom/aliases.zsh'
+alias editAlias='code ~/.oh-my-zsh/custom/aliases.zsh'
+
+alias editGitAlias='code ~/.gitconfig'
